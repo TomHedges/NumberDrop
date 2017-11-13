1a403312e99d4ab120e9fdd002ed991b3b60b566
Tom Hedges <tom@tomhedges.com>
2017-11-13 18:13:18 +0000

    Initial commit
    Signed-off-by: Tom Hedges <tom@tomhedges.com>

---
+++ app/src/main/java/com/tomhedges/numberdrop/DroppingNumbers.java
@@ -0,0 +1,13 @@
+package com.tomhedges.numberdrop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DroppingNumbers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dropping_numbers);
+    }
+}
